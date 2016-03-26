6a13ffc416c8ea26e079cb5efad5419bd5f2e227
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-03-26 23:53:30 +0800

    drop duplicate key
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- project.clj
+++ project.clj
@@ -7,9 +7,6 @@
 
   :signing {:gpg-key "C079AE25"}
 
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.8.34"]]
 
