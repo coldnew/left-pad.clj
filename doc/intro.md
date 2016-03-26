b7bded1e8bb4bcfb7af563f0cdfd7048380fd964
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-03-26 22:49:09 +0800

    first commit
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to left-pad
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/left_pad/core_test.clj
@@ -0,0 +1,7 @@
+(ns left-pad.core-test
+  (:require [clojure.test :refer :all]
+            [left-pad.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
